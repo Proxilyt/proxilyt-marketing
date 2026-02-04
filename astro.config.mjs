79162204baa5884b7c3afd35a8b3fa6942ae23e5
hga50 <himank_goel@sfu.ca>
2026-02-03 23:29:29 -0800

    added basic layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,16 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+import tailwind from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.proxilyt.com',
-    base: '/',
-    output: 'static',
-    integrations: [sitemap()],
+  site: 'https://www.proxilyt.com',
+  base: '/',
+  output: 'static',
+  integrations: [sitemap()],
+
+  vite: {
+    plugins: [tailwind()],
+  },
 });
